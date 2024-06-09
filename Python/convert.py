fb52ec96ba36b0555956d0f82166c13051920577
RandallMcCutcheon <randall.mccutcheon@gmail.com>
2024-06-09 17:39:19 +0930

    Update convert.py
    Up to R22 complete

--- Python/convert.py
+++ Python/convert.py
@@ -1,7 +1,7 @@
 """ Imports """"
 import numpy as np
 
-def unbiased_pol2cart(range, bearing, range_sd, bearing_sd):
+def unbiased_pol2cart(r, beta, sd_r, sd_beta):
     """
     Date: 14 Dec 2020
     Author: Dr Sanjeev Arulampalam
@@ -14,33 +14,29 @@ def unbiased_pol2cart(range, bearing, range_sd, bearing_sd):
     Inputs:
         r - range
         beta - Bearing
-        var_r - Standard Deviation of Range
-        var_beta - Standard Deviation of Bearing
+        sd_r - Standard Deviation of Range
+        sd_beta - Standard Deviation of Bearing
 
     Outputs:
         z -
         R -
 
-    9 Jun - modifications include ability to input arrays
+    9 Jun - modifications include the ability to input arrays
     """
   
     # Equation 12
-    lambda_beta = np.exp(-bearing_sd / 2)
-    lambda_beta2 = np.power(lambda_beta1, 4)
+    lambda_beta = np.exp(-np.power(sd_beta,2) / 2)
+    lambda_beta_prime = np.power(lambda_beta, 4)
 
-    inv_lambda_beta1 = 1 / lambda_beta1
-    inv_lambda_beta2 = 1 / lambda_beta2
+    inv_lambda_beta = 1 / lambda_beta
+    inv_lambda_beta_prime = 1 / lambda_beta_prime
 
-    xm_u = inv_lambda_beta1 * r * np.cos(beta)
-    ym_u = inv_lambda_beta1 * r * np.sin(beta)
+    xm_u = inv_lambda_beta * r * np.cos(bearing)
+    ym_u = inv_lambda_beta * r * np.sin(bearing)
 
-    R11 = (np.power(inv_lambda_beta1, 2) - 2) * np.power(r, 2) * np.power(
-        np.cos(beta), 2
-    ) + 0.5 * (np.power(r, 2) + var_r) * (1 + lambda_beta2 * np.cos(2 * beta))
+    R11 = (np.power(lambda_beta, -2) - 2) * np.power(r, 2) * np.power(np.cos(beta), 2) + 0.5 * (np.power(r, 2) + np.power(sd_r, 2)) * (1 + lambda_beta_prime * np.cos(2 * beta))
 
-    R22 = (np.power(inv_lambda_beta1, 2) - 2) * np.power(r, 2) * np.power(
-        np.sin(beta), 2
-    ) + 0.5 * (np.power(r, 2) + var_r) * (1 - lambda_beta2 * np.cos(2 * beta))
+    R22 = (np.power(inv_lambda_beta1, 2) - 2) * np.power(r, 2) * np.power(np.sin(beta), 2) + 0.5 * (np.power(r, 2) + var_r) * (1 - lambda_beta2 * np.cos(2 * beta))
 
     R12 = (np.power(inv_lambda_beta1, 2) - 2) * np.power(r, 2) * np.cos(beta) * np.sin(
         beta
