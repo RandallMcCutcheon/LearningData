fb6e983d6c960e9ca900457d6cf2281981cc4a9a
RandallMcCutcheon <randall.mccutcheon@gmail.com>
2024-06-10 08:22:13 +0930

    Update convert.py

--- Python/convert.py
+++ Python/convert.py
@@ -9,8 +9,10 @@ def unbiased_pol2cart(r, beta, sd_r, sd_beta):
     
     This is the unbiased polar to Cartesian conversion based on Longbin Mo's
     paper IEEE Transactions on Aerospace and Electronic Systems,
-    Vol 34. No. 3, July 1998, p1023-1027.
+    Vol 34. No. 3, July 1998, p1023-1027
 
+    RM note: Title of the paper is: Unbiased converted measurements for tracking
+    
     Inputs:
         r - range
         beta - Bearing
@@ -31,16 +33,18 @@ def unbiased_pol2cart(r, beta, sd_r, sd_beta):
     inv_lambda_beta = 1 / lambda_beta
     inv_lambda_beta_prime = 1 / lambda_beta_prime
 
+    # Equation 5
     xm_u = inv_lambda_beta * r * np.cos(bearing)
     ym_u = inv_lambda_beta * r * np.sin(bearing)
 
+    # Equation 7a
     R11 = (np.power(lambda_beta, -2) - 2) * np.power(r, 2) * np.power(np.cos(beta), 2) + 0.5 * (np.power(r, 2) + np.power(sd_r, 2)) * (1 + lambda_beta_prime * np.cos(2 * beta))
 
-    R22 = (np.power(inv_lambda_beta1, 2) - 2) * np.power(r, 2) * np.power(np.sin(beta), 2) + 0.5 * (np.power(r, 2) + var_r) * (1 - lambda_beta2 * np.cos(2 * beta))
+    # Equation 7b
+    R22 = (np.power(lambda_beta, -2) - 2) * np.power(r, 2) * np.power(np.sin(beta), 2) + 0.5 * (np.power(r, 2) + np.power(sd_r, 2)) * (1 - lambda_beta_prime * np.cos(2 * beta))
 
-    R12 = (np.power(inv_lambda_beta1, 2) - 2) * np.power(r, 2) * np.cos(beta) * np.sin(
-        beta
-    ) + 0.5 * (np.power(r, 2) + var_r) * lambda_beta2 * np.sin(2 * beta)
+    # Equation 7c
+    R12 = (np.power(lambda_beta, -2) - 2) * np.power(r, 2) * np.cos(beta) * np.sin(beta) + 0.5 * (np.power(r, 2) + np.power(sd_r, 2)) * lambda_beta_prime * np.sin(2 * beta)
 
     z = np.transpose([xm_u, ym_u])
     R = np.array([[R11, R12], [R12, R22]])
